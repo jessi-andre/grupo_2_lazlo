5bb853be8a4f7fff23f9bb36614806da868343dc
nathgaitan <nathalia2912dante@gmail.com>
2021-11-10 19:26:48 -0300

    json de comentarios

--- sitio/controllers/indexController.js
+++ sitio/controllers/indexController.js
@@ -1,3 +1,10 @@
+
+const fs = require('fs');
+const path = require('path');
+
+let comentarios = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data', 'comentarios.json'), 'utf-8'))
+
+
 module.exports = {
     index: (req,res) => {
         return res.render('home')
@@ -9,6 +16,22 @@ module.exports = {
         return res.render('historia')
     },
     comunidad: (req,res) => {
-        return res.render('comunidad')
+        return res.render('comunidad',{comentarios})
+        
     },
-}
+    guardarComentario: (req, res) => {
+        //return res.send(req.body);
+        const {comentario} = req.body;
+
+        let coment = {
+            id: comentarios[comentarios.length - 1].id + 1,
+            nombre:req.session.loginUsuario.first_name,
+            comentario
+        }
+        comentarios.push(coment)
+
+        fs.writeFileSync(path.join(__dirname, '..', 'data', 'comentarios.json'), JSON.stringify(comentarios, null, 3), 'utf-8');
+        return res.redirect('/comunidad');
+        }
+
+  }
