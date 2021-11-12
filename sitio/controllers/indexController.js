9bd0c07ecccedbbcd4af31718ac42df392f207b0
jessi-andre <jessicadaianaandre@gmail.com>
2021-11-12 19:09:35 -0300

    Creación de busqueda

--- sitio/controllers/indexController.js
+++ sitio/controllers/indexController.js
@@ -1,14 +1,45 @@
+const db = require('../database/models')
+const { Op } = require('sequelize');
+
 module.exports = {
-    index: (req,res) => {
+    index: (req, res) => {
         return res.render('home')
     },
-    carrito: (req,res) => {
+    carrito: (req, res) => {
         return res.render('carrito')
     },
-    historia: (req,res) => {
+    historia: (req, res) => {
         return res.render('historia')
     },
-    comunidad: (req,res) => {
+    comunidad: (req, res) => {
         return res.render('comunidad')
     },
+    search: (req, res) => {
+        db.Product
+            .findAll({
+                include: [
+                    "colors",
+                    "size",
+                    "category"
+                ],
+                where: {
+                    [Op.or]: [
+                        {
+                            name: { [Op.substring]: req.query.busqueda }
+                        },
+                        {
+                            description: { [Op.substring]: req.query.busqueda }
+                        },
+                    ]
+                }
+            })
+            .then(products => {
+                return res.render('search', {
+                    products,
+                    busqueda: req.query.busqueda
+                })
+            })
+            .catch(error => console.log(error))
+
+    }
 }
--- sitio/routes/index.js
+++ sitio/routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const {index,carrito,historia,comunidad} = require('../controllers/indexController')
+const {index,carrito,historia,comunidad, search} = require('../controllers/indexController')
 
 /* GET home page. */
 router.get('/',index );
 router.get('/carrito',carrito);
 router.get('/historia',historia);
 router.get('/comunidad',comunidad);
+router.get('/search', search);
+
 
 
 
