384037d109797b49d7ecd878020480404d4c51d6
nathgaitan <nathalia2912dante@gmail.com>
2021-12-17 12:14:21 -0300

    sweetalert

--- sitio/public/javascripts/sweetalerts.js
+++ sitio/public/javascripts/sweetalerts.js
@@ -37,25 +37,63 @@
         }
       }) 
 })*/
-document.getElementById("eliminarProducto").addEventListener("submit",  (e)=>{
 
-    e.preventDefault () 
-Swal.fire({
-    title: 'Are you sure?',
-    text: "You won't be able to revert this!",
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Yes, delete it!'
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire(
-        'Deleted!',
-        'Your file has been deleted.',
-        'success'
-      )
-      document.getElementById("eliminarProducto").submit()
-    }
-  })
-   })
+const confirm = (id, e) => {
+  console.log(e.target)
+      e.preventDefault () 
+      const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: 'btn btn-success',
+          cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+      })
+      
+      swalWithBootstrapButtons.fire({
+        title: 'Esta seguro que quiere eliminar el producto?',
+        text: "No se puede revertir!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Si, eliminalo!',
+        cancelButtonText: 'No, no lo elimines!',
+        reverseButtons: true
+      }).then((result) => {
+        if (result.isConfirmed) {
+          swalWithBootstrapButtons.fire(
+            'Producto Eliminado',
+            'El producto se elimino correctamente',
+            'Muy bien!'
+          )
+          document.getElementById(id).submit()
+        } else if (
+          // Read more about handling dismissals below 
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire(
+            'Cancelled',
+            'Your imaginary file is safe :)',
+            'error'
+          )
+        }
+      }) 
+
+  /*Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Deleted!',
+          'Your file has been deleted.',
+          'success'
+        )
+        document.getElementById(id).submit()
+      }
+    })*/
+}
+
